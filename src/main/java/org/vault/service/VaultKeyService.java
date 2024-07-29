c4bef008cb1c12a02ace8018938a50779bad3137
true-false-try <6mateo6ch@gmail.com>
2024-07-29 13:48:56 +0300

    [VAULT-3] - changed logic return auth from Redis

--- src/main/java/org/vault/service/VaultKeyService.java
+++
@@ -1,9 +0,0 @@
-package org.vault.service;
-
-import org.vault.exception.KeyValueReaderException;
-
-import java.io.IOException;
-
-public interface VaultKeyService {
-    String getKey() throws IOException, KeyValueReaderException;
-}
